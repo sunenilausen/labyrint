37b5bc076246ffece706f77963672f4f95d20b15
Center Nulet <Center Nulet>
2017-05-03 19:03:36 +0200

    lektion3

---
+++ Assets/Scripts/WallBoop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBoop : MonoBehaviour {
+	AudioSource audio;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "player") 
+		{
+			audio.Play();
+		}	
+	}
+}
+
+ 
